6ae8d638b218480e5be035d5c92cdcd79fa0dabe
DU <dubydu97@gmail.com>
2022-07-30 21:57:24 +0700

    feat: int32-to-ipv4

--- 27-july/code-war.playground/Pages/int32-to-ipv4.xcplaygroundpage/Contents.swift
+++ 27-july/code-war.playground/Pages/int32-to-ipv4.xcplaygroundpage/Contents.swift
@@ -2,6 +2,26 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+/// https://www.codewars.com/kata/52e88b39ffb6ac53a400022e/swift
+
+extension UInt32 {
+    func toI8Arr() -> [UInt8] {
+        return [
+            UInt8((self & 0xFF000000) >> 24),
+            UInt8((self & 0x00FF0000) >> 16),
+            UInt8((self & 0x0000FF00) >> 8),
+            UInt8(self & 0x000000FF)
+        ]
+    }
+}
+
+func ipv4(of i32: UInt32) -> String {
+    let i8List = i32.toI8Arr()
+    return i8List
+        .map { "\($0)" }
+        .joined(separator: ".")
+}
+
+ipv4(of: 2149583361)
 
 //: [Next](@next)
