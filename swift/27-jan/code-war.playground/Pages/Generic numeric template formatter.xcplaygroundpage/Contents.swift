b154a60c3b9fde964b67cc130b8a9d759aa5906b
dubydu <dubydu97@gmail.com>
2024-01-29 12:10:44 +0700

    feat: generic numeric template formatter

--- swift/27-jan/code-war.playground/Pages/Generic numeric template formatter.xcplaygroundpage/Contents.swift
+++ swift/27-jan/code-war.playground/Pages/Generic numeric template formatter.xcplaygroundpage/Contents.swift
@@ -1,34 +1,28 @@
 import Foundation
 
-/// [WIP] : https://www.codewars.com/kata/59901fb5917839fe41000029/train/swift
+/// https://www.codewars.com/kata/59901fb5917839fe41000029/train/swift
 
-func numericFormatter(_ template: String, _ numbers: String = "1234567890") -> String {
-    let templateChars = template.map { "\($0)" }
-    var numberChars = numbers.map { "\($0)" }
-    var res = ""
-    let characterset = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    for (i, v) in templateChars.enumerated() {
-        if (v == " ") {
-            res += " "
-            numberChars.insert(" ", at: i)
-        } else if (v.trimmingCharacters(in: .whitespacesAndNewlines)
-            .rangeOfCharacter(from: characterset.inverted) != nil) {
-            // numberChars.insert("", at: i)
-            res += v
-        } else {
-            if (numbers.count <= i) {
-                numberChars.append(numberChars[i - numbers.count])
-            }
-            res += numberChars[i]
-        }
-    }
-    return res
+func isAlphabet(_ input: String) -> Bool {
+    return (input.trimmingCharacters(in: .whitespacesAndNewlines)
+        .rangeOfCharacter(from: CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").inverted) == nil)
 }
 
-// numericFormatter("xxx xxxxx xx", "5465253289")
-// numericFormatter("xxx xxxxx xx")
-// numericFormatter("+555 aaaa bbbb", "18031978")
-numericFormatter("D6jnDviO1banyqy26187920")
+func numericFormatter(_ template: String, _ numbers: String = "1234567890") -> String {
+    var templateChars = template.map { "\($0)" }
+    let numberChars = numbers.map { "\($0)" }
+    var index = 0
+    for (idx, char) in templateChars.enumerated() {
+        if (char != " " && isAlphabet(char)) {
+            if !(index < numberChars.count) {
+                index = 0
+            }
+            templateChars[idx] = numberChars[index]
+            index += 1
+        }
+    }
+    return templateChars.joined()
+}
 
-// 1623456718901226187920
-// 16234567189012326187920
+isAlphabet(" ")
+
+numericFormatter("+xxxx yyyy zzzz")
